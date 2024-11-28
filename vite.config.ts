0edc2c38cdb9edef0e1546eac76645d356b3f4af
Waleed Zaryab <westendcrew1982@gmail.com>
2024-11-28 22:31:33 +1100

    Fix deploy error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,4 +12,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   }
-});
+})
