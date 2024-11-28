e610f2ca633f1439059b8e4632cebc3c30f19a5b
Waleed Zaryab <westendcrew1982@gmail.com>
2024-11-28 19:37:48 +1100

    Added code for Candidate Interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,20 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// Interface for the Candidate objects returned by the API
+export interface IGithubUser {
+    login: string;
+    id: number;
+    avatar_url: string;
+    html_url: string;
+  }
+//   Made by Waleed
+  export interface ICandidate extends IGithubUser {
+    name: string | null;
+    location: string | null;
+    email: string | null;
+    company: string | null;
+    bio: string | null;
+    created_at: string;
+    updated_at: string;
+    followers: number;
+    following: number;
+    public_repos: number;
+  }
